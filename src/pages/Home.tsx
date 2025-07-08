6f68ace9888455b143cea8a785ce910c0444d213
Joshua-Gaspar <joshuagaspar03@gmail.com>
2025-07-08 16:05:03 -0500

    starter commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+  return (
+
+    <div className="min-h-screen ">
+      {/* theme toggle */}
+
+      {/* background effect*/}
+
+      {/* navbar */}
+
+      {/* main content*/}
+
+      {/* Footer */}
+    </div>
+  );
+};
