38aa8983da14f930c0d1ccc3acc86a578fea0285
Joshua-Gaspar <joshuagaspar03@gmail.com>
2025-07-10 14:37:41 -0500

    Implement navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
+import { Navbar } from "@/components/Navbar";
 import { StarBackground } from "@/components/StarBackground";
 import { ThemeToggle } from "@/components/ThemeToggle";
 
 export const Home = () => {
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* theme toggle */}
       <ThemeToggle />
       {/* background effect*/}
       <StarBackground />
       {/* navbar */}
-
+      <Navbar />
       {/* main content*/}
 
       {/* Footer */}
